2c91cda69413aba0d261ea9417cb18deec1cc239
yek@gmail.com <surgandaalif@gmail.com>
2023-06-08 13:25:42 +0700

    tugas55

--- tugas55.cy.js
+++ tugas55.cy.js
@@ -4,44 +4,41 @@ describe('Working with input', () => {
   it("Visit the website", () => {
     cy.visit('http://zero.webappsecurity.com/bank/pay-bills.html')
     //cy.url().should('include', 'pay-bills.html')
-  
-   /* cy.get('#user_login').clear()
-    cy.get('#user_login').type('username')
-  
-    cy.get('input[name="user_password"]').clear()
-    cy.get('input[name="user_password"]').type('password')
     
-    cy.get('#user_remember_me').should('have.prop', 'checked')
-    */
-    
+    /* cy.get('#user_login').clear()
+     cy.get('#user_login').type('username')
    
-      cy.fixture('user').then(user => {
-        const username = user.username
-        const password = user.password
-        const amount = user.amount
-        const date = user.date
-        const description = user.description
-      
-        cy.tugas55(username, password)
-        
-        cy.get('#pay_bill_tab').click()
-        cy.get('h2').should('contain.text', 'Make payments to your saves payees')
-        cy.tugas5(amount, date, description)
+     cy.get('input[name="user_password"]').clear()
+     cy.get('input[name="user_password"]').type('password')
      
-        /*cy.get('#user_login').clear()
-        cy.get('#user_login').type(username)
-        
-        cy.get('input[name="user_password"]').clear()
-        cy.get('input[name="user_password"]').type(password)*/
-  
-       // cy.get('.btn-primary').click()
-  
-       // cy.get('.alert-error').should('contain.text', 'Login and/or password are wrong.')
-    })
-    
-    
+     cy.get('#user_remember_me').should('have.prop', 'checked')
+     */
+   
+    cy.fixture('user').then(user => {
+      const username = user.username
+      const password = user.password
+      const amount = user.amount
+      const date = user.date
+      const description = user.description
+       
+      cy.tugas55(username, password)  // 5. ========> nama custom commands nya ikutin yang ada di file commands.js ==> misal 'login' tadi
+          
+      cy.get('#pay_bills_tab').click()
+      cy.get('h2').should('contain.text', 'Make payments to your saves payees')
+          
+          
+      cy.tugas5(amount, date, description)  // 6. ========> nama custom commands nya ikutin yang ada di file commands.js ==> misal 'paybills' tadi
+      
 
+      /*cy.get('#user_login').clear()
+      cy.get('#user_login').type(username)
+      
+      cy.get('input[name="user_password"]').clear()
+      cy.get('input[name="user_password"]').type(password)*/
+    
+      // cy.get('.btn-primary').click()
+    
+      // cy.get('.alert-error').should('contain.text', 'Login and/or password are wrong.')
     });
+  });
 });
-
-
